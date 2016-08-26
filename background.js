6215058542f7805ac7ccab5d07a607677973d015
Eric Lawrence <ericlaw1979@hotmail.com>
2016-08-26 00:15:07 -0500

    New art, OS X bugfix

--- background.js
+++ background.js
@@ -5,35 +5,36 @@ const sMinorVer  = /Chrome\/[0-9]+\.[0-9]+\.([0-9]+)/.exec(navigator.userAgent)[
 const allColors = ["purple", "blue", "green", "black"];
 const bIsMac = navigator.userAgent.includes("Mac OS X");
 
-var myColor = allColors[sMajorVer % allColors.length];
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext('2d');
-ctx.font = "14px Verdana";
-ctx.strokeStyle = myColor;
-ctx.strokeText(sMajorVer, 1, 11);
+const myColor = allColors[sMajorVer % allColors.length];
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
 
-if (bIsMac)
-{
+var imageData;
+if (bIsMac) {
 /* On OS X we need to draw our own minor version
    text because the badge truncates. It's unappealing to
    do this in general because on Windows at least, using
    the badge gives us a bit more vertical space. */
-    // ctx.fillStyle = "white";
-    // ctx.fillRect(0,12,19,19);
-    ctx.font = "9px Helvetica Neue";
+
+    ctx.font = "bold 28px Verdana";
     ctx.fillStyle = myColor;
-    ctx.fillText(sMinorVer, 0, 19);
+    ctx.fillText(sMajorVer, 0, 22);
+    ctx.font = "18px Arial Narrow";
+    ctx.fillText(sMinorVer, 3, 38);
+    imageData = ctx.getImageData(0, 0, 38, 38);
+}
+else {
+    ctx.font = "14px Verdana";
+    ctx.strokeStyle = myColor;
+    ctx.strokeText(sMajorVer, 1, 11);
+    imageData = ctx.getImageData(0, 0, 19, 19);
 }
 
-var imageData = ctx.getImageData(0, 0, 19, 19);
-chrome.browserAction.setIcon({
-  imageData: imageData
-});
 try {
-    chrome.browserAction.setTitle( { title: navigator.userAgent.replace("Mozilla/5.0", "") });
-    if (!bIsMac)
-    {
-        chrome.browserAction.setBadgeText( {text: sMinorVer} );     // BUG: Truncates on OS X
+    chrome.browserAction.setIcon({ imageData: imageData });
+    chrome.browserAction.setTitle({ title: navigator.userAgent.replace("Mozilla/5.0", "") });
+    if (!bIsMac) {
+        chrome.browserAction.setBadgeText( {text: sMinorVer} );
         chrome.browserAction.setBadgeBackgroundColor({color: myColor});
     }
-} catch (e) { alert("Error: " + e); }
+} catch (e) { console.log(e); }
