3d66fb545f810c7c495d087c5b286ddf21b09eec
Greg Whitworth <gwhit@microsoft.com>
2019-03-22 15:17:16 -0700

    Added link to regressionf Found=<Major Version> of Chromium

--- popup.js
+++ popup.js
@@ -2,16 +2,7 @@
 
 if (typeof chrome.runtime === "undefined") chrome = browser;
 
-document.addEventListener('DOMContentLoaded', function() {
-        chrome.runtime.getPlatformInfo(function (o) {
-            const arrVer = / Edg\/([0-9.]+)/.exec(navigator.userAgent);
-            let sEdge = (arrVer && arrVer.length > 0) ? arrVer[1] : "";
-            if (sEdge) (sEdge = "Edge v" + sEdge + "; Chromium v");
-            document.getElementById("txtStatus").textContent = sEdge +
-            /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1] + "\n" +
-            JSON.stringify(o);
-        });
-
+document.addEventListener('DOMContentLoaded', function() {        
         const lnkMoreInfo = document.getElementById("lnkMoreInfo");
         lnkMoreInfo.addEventListener("click", function() { chrome.tabs.create({url: "chrome://version/?show-variations-cmd"}); }, false);
 
@@ -22,16 +13,28 @@ document.addEventListener('DOMContentLoaded', function() {
         lnkSysInfo.addEventListener("click", function() { chrome.tabs.create({url: "chrome://system/"}); }, false);
 
         const lnkCopyForBug = document.getElementById("lnkCopyForBug");
-        lnkCopyForBug.addEventListener("click", function() { copyForBug(); }, false);
+        lnkCopyForBug.addEventListener("click", function() { copyForBug(); }, false);        
 
         if (navigator.userAgent.indexOf(" Edg/") > -1) {
           document.getElementById("txtTitle").textContent = "Edge Version";
           document.getElementById("lnkMoreInfo").textContent = "Full Edge Info";
         }
 
+        chrome.runtime.getPlatformInfo(function (o) {
+            const arrVer = / Edg\/([0-9.]+)/.exec(navigator.userAgent);
+            let sEdge = (arrVer && arrVer.length > 0) ? arrVer[1] : "";
+            if (sEdge) (sEdge = "Edge v" + sEdge + "; Chromium v");
+            let data = sEdge + /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1] + "\n" + JSON.stringify(o);
+            document.getElementById("txtStatus").textContent = data;
+
+            const majorVersion = arrVer[1].match(/(\d{1,3})/g)[0];
+            const lnlnkRegressionsForThisVersion = document.getElementById("lnkRegressionsForThisVersion");
+            let href = `https://bugs.chromium.org/p/chromium/issues/list?can=2&q=FoundIn%3D${majorVersion}+`;     
+            lnlnkRegressionsForThisVersion.addEventListener("click", function() { chrome.tabs.create({url: href });}, false);
+        });
 }, false);
 
-function copyForBug()
+function copyForBug() 
 {
     const copyFrom = document.createElement("textarea");
 
@@ -46,4 +49,3 @@ function copyForBug()
     lnkCopyForBug.textContent = "copied!";
     setTimeout(function() { lnkCopyForBug.innerHTML = "Copy"; }, 450);
 }
-
