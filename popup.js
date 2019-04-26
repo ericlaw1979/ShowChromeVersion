3b6dd6316efee8950b51f6c3e66e6d90380077b4
Eric Lawrence <ericlaw1979@hotmail.com>
2019-04-26 11:19:22 -0500

    Fix Find Regressions link in Chrome
    Also, drop nacl_arch from info.

--- popup.js
+++ popup.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const lnkCopyForBug = document.getElementById("lnkCopyForBug");
         lnkCopyForBug.addEventListener("click", function() { copyForBug(); }, false);
 
-        if (navigator.userAgent.indexOf(" Edg/") > -1) {
+        if (navigator.userAgent.includes(" Edg/")) {
           document.getElementById("txtTitle").textContent = "Edge Version";
           document.getElementById("lnkMoreInfo").textContent = "Full Edge Info";
         }
@@ -24,10 +24,11 @@ document.addEventListener('DOMContentLoaded', function() {
             const arrVer = / Edg\/([0-9.]+)/.exec(navigator.userAgent);
             let sEdge = (arrVer && arrVer.length > 0) ? arrVer[1] : "";
             if (sEdge) (sEdge = "Edge v" + sEdge + "; Chromium v");
-            let data = sEdge + /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1] + "\n" + JSON.stringify(o);
+            const verChrome = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
+            let data = sEdge + verChrome + "\n" + JSON.stringify(o, (k,v) => {if (k==="nacl_arch") return undefined; return v; } );
             document.getElementById("txtStatus").textContent = data;
 
-            const majorVersion = arrVer[1].match(/(\d{1,3})/g)[0];
+            const majorVersion = verChrome.match(/(\d{1,3})/g)[0];
             const lnlnkRegressionsForThisVersion = document.getElementById("lnkRegressionsForThisVersion");
             let href = `https://bugs.chromium.org/p/chromium/issues/list?can=2&q=FoundIn%3D${majorVersion}+Type%3DBug-Regression`;     
             lnlnkRegressionsForThisVersion.addEventListener("click", function() { chrome.tabs.create({url: href });}, false);
