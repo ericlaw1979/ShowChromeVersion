01844f980d984a49766383dcb576bbfa534b96a9
Eric Lawrence <ericlaw1979@hotmail.com>
2021-07-01 10:16:23 -0500

    Support dark mode colors, remove Mac hackery
    We no longer need special code for mac.

--- background.js
+++ background.js
@@ -10,43 +10,32 @@ function updateUI() {
       sMajorVer = /Edg\/([0-9]+)/.exec(navigator.userAgent)[1];
       sMinorVer  = /Edg\/[0-9]+\.[0-9]+\.([0-9]+)/.exec(navigator.userAgent)[1];
     }
-    const allColors = ["purple", "blue", "green", "black"];
-    const bIsMac = navigator.userAgent.includes("Mac OS X");
 
-    const myColor = allColors[sMajorVer % allColors.length];
+    // Select color palette based on dark/light theme. Use light colors on
+    // Dark Mode and vice-versa.
+    const bDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const arrDarkColors = ["purple", "blue", "green", "black"];
+    const arrColors = bDarkMode ? ["fuchsia", "aqua", "lime", "white"] 
+                    : arrDarkColors;
+
+    const myColor = arrColors[sMajorVer % arrColors.length];
+    const myBadgeColor = arrDarkColors[sMajorVer % arrDarkColors.length];
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
 
     var imageData;
-    if (bIsMac) {
-    /* On OS X we need to draw our own minor version
-       text because the badge truncates. It's unappealing to
-       do this in general because on Windows at least, using
-       the badge gives us a bit more vertical space. */
-
-        ctx.font = "bold 28px Verdana";
-        ctx.fillStyle = myColor;
-        ctx.fillText(sMajorVer, 0, 22);
-        ctx.font = "18px Arial Narrow";
-        ctx.fillText(sMinorVer, 3, 38);
-        imageData = ctx.getImageData(0, 0, 38, 38);
-        chrome.browserAction.setBadgeText( {text: ""} );
-    }
-    else {
-        ctx.font = "14px Verdana";
-        ctx.strokeStyle = myColor;
-        ctx.strokeText(sMajorVer, 1, 11);
-        imageData = ctx.getImageData(0, 0, 19, 19);
-    }
+    ctx.font = "14px Verdana";
+    ctx.strokeStyle = myColor;
+    ctx.strokeText(sMajorVer, 1, 11);
+    imageData = ctx.getImageData(0, 0, 19, 19);
 
     try {
         chrome.browserAction.setIcon({ imageData: imageData });
         chrome.browserAction.setTitle({ title: navigator.userAgent.replace("Mozilla/5.0", "") });
-        if (!bIsMac) {
-            chrome.browserAction.setBadgeText( {text: sMinorVer} );
-            chrome.browserAction.setBadgeBackgroundColor({color: myColor});
-        }
-    } catch (e) { console.error(e); }
+        chrome.browserAction.setBadgeText( {text: sMinorVer} );
+        chrome.browserAction.setBadgeBackgroundColor({color: myBadgeColor});
+    }
+    catch (e) { console.error(e); }
 }
 
 // There's a pending update available; user must restart the browser.
